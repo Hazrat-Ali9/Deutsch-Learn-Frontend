3621d066e0a9c5e63b3bde35e8fb2dd800864f3e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-15 01:39:22 +0600

    layout page  🚃

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Choose from "@/Components/Choose";
 import Faq from "@/Components/Faq";
 import Slider from "@/Components/Slider";
 import Timeline from "@/Components/Timeline";
-
+// Layout page
 export default function Home() {
   return (
     <div>
